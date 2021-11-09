3fc6aeb69ef0d2eb5cf8943d0345392a8446edd6
YannickMontes <montes.yannick@gmail.com>
2021-11-10 00:49:04 +0100

    Add signature functions

--- server.js
+++ server.js
@@ -19,17 +19,19 @@ io.on("connection", socket => {
     //Penser a conserver le socket pour pouvoir s'en servir plus tard
     //Remplacer les callbacks par des fonctions dans d'autres fichiers.
 
-    socket.on("@authenticate", () => {});
-    socket.on("@getUsers", () => {});
-    socket.on("@getOrCreateOneToOneConversation", () => {});
-    socket.on("@createManyToManyConversation", () => {});
-    socket.on("@getConversations", () => {});
-    socket.on("@postMessage", () => {});
-    socket.on("@seeConversation", () => {});
-    socket.on("@replyMessage", () => {});
-    socket.on("@editMessage", () => {});
-    socket.on("@reactMessage", () => {});
-    socket.on("@deleteMessage", () => {});
+    socket.on("@authenticate", ({ username, password }, callback) => { callback({code:"SUCCESS", data:{}}); });
+    
+    socket.on("@getUsers", ({token}, callback) => { callback({code:"SUCCESS", data:{}}); });
+    socket.on("@getOrCreateOneToOneConversation", ({token, username}, callback) => { callback({code:"SUCCESS", data:{}});  });
+    socket.on("@createManyToManyConversation", ({token, usernames}, callback) => {callback({code:"SUCCESS", data:{}});});
+    socket.on("@getConversations", ({token}, callback) => {callback({code:"SUCCESS", data:{}});});
+    
+    socket.on("@postMessage", ({token, conversation_id, content}, callback) => {callback({code:"SUCCESS", data:{}});});
+    socket.on("@seeConversation", ({token, conversation_id, message_id}, callback) => {callback({code:"SUCCESS", data:{}}); });
+    socket.on("@replyMessage", ({token, conversation_id, message_id, content}, callback) => {callback({code:"SUCCESS", data:{}});});
+    socket.on("@editMessage", ({token, conversation_id, message_id, content}, callback) => {callback({code:"SUCCESS", data:{}});});
+    socket.on("@reactMessage", ({token, conversation_id, message_id, reaction}) => {callback({code:"SUCCESS", data:{}});});
+    socket.on("@deleteMessage", ({token, conversation_id, message_id, content}) => {callback({code:"SUCCESS", data:{}});});
 
     socket.on("disconnect", (reason) =>{ });
 });
